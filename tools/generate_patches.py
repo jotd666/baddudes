2a93c1c11546286511b618dafffe17dd4e6ef21f
Jean-François Fabre <jotd666@gmail.com>
2024-12-16 23:22:06 +0100

    improving patch generator

--- tools/generate_patches.py
+++ tools/generate_patches.py
@@ -13,6 +13,8 @@ relocated_ram_offsets = {}
 def is_in_ram(ext_address):
     return 0xff8000 <= ext_address <= 0x00ffc000
 
+special_read = {0x300000,0x300001,0x300008,0x300009} | set(range(0x30c000, 0x30c010))
+
 for line in af.lines:
     inst_info = ira_asm_tools.parse_instruction_line(line)
     if inst_info:
@@ -25,13 +27,28 @@ for line in af.lines:
                 else:
                     offset = 2 if i==0 else inst_info["size"]-4
                 prefix,*rest = arg.split("_")
-                if prefix=="ext" or prefix=="ram":
-                    ext_address = int(rest[0],16)
-                    if is_in_ram(ext_address):
-                        # in RAM: relocate
-                        reloc_address = inst_info["address"]
-                        relocated_ram_offsets[(reloc_address,offset)] = arg
+                if rest:
+                    try:
+                        ext_address = int(rest[-1],16)
 
+                        if is_in_ram(ext_address):
+                            # in RAM: relocate
+                            reloc_address = inst_info["address"]
+                            relocated_ram_offsets[(reloc_address,offset)] = arg
+                        elif ext_address >= 0x240000:
+                            instruction = inst_info["instruction"]
+                            if instruction.startswith("MOVE."):
+                                size = instruction[-1]
+                                # laying out 4 traps for read/write byte/word
+                                if i==0:
+                                    # read
+                                    if ext_address in special_read:
+                                        print("special read",line)
+                                else:
+                                    # write
+                                    print("special write",line)
+                    except ValueError:
+                        pass
 with open(src_dir / "patchlist.68k","w") as f:
     f.write("ram_relocs:\n")
     for (reloc,offset),v in sorted(relocated_ram_offsets.items()):
