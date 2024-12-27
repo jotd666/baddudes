a53ef8fe9e41f4d435cc6cea5b1de1d7b1d837fb
Jean-François Fabre <jotd666@gmail.com>
2024-12-27 23:45:37 +0100

    coded a memory allocator & file loader

--- assets/amiga/convert_dudes_pics.py
+++ assets/amiga/convert_dudes_pics.py
@@ -3,14 +3,31 @@ import os,sys,bitplanelib,subprocess,json,pathlib
 
 this_dir = pathlib.Path(__file__).absolute().parent
 
-data_dir = os.path.join(this_dir,"..","..","data")
-src_dir = os.path.join(this_dir,"..","..","src","amiga")
+data_dir = this_dir / ".." / ".." / "data"
+src_dir = this_dir / ".." / ".." / "src" / "amiga"
 
-for i in range(2):
-    dude_pic = this_dir / f"dudes_{i}.png"
-    p = Image.open(dude_pic)
-    transparent = p.getpixel((0,0))
-    x_start,rval = bitplanelib.autocrop_x(p,transparent,align=16)
-    y_start,rval = bitplanelib.autocrop_y(rval,transparent)
-    print(x_start,y_start)
-    rval.save(this_dir/ f"dudes_crop_{i}.png")
+with open(src_dir / "dudes.68k","w") as f:
+    f.write("main_table:\n")
+    for i in range(2):
+        f.write(f"\tdc.l\tdude_{i}-main_table\n")
+
+    for i in range(2):
+        dude_pic = this_dir / f"dudes_{i}.png"
+        f.write(f"dude_{i}:\n")
+        p = Image.open(dude_pic)
+        transparent = p.getpixel((0,0))
+        x_start,rval = bitplanelib.autocrop_x(p,transparent,align=16)
+        y_start,rval = bitplanelib.autocrop_y(rval,transparent)
+        width = rval.size[0]
+        height = rval.size[1]
+
+        f.write(f"\tdc.w\t{x_start},{y_start},{width},{height}\n")
+        p = bitplanelib.palette_extract(rval)
+        p.remove(transparent)
+        p.insert(0,transparent)
+        f.write("; palette\n")
+        bitplanelib.palette_dump(p,f)
+        f.write("; bitmap\n")
+        raw = bitplanelib.palette_image2raw(rval,None,p,mask_color=transparent)
+        bitplanelib.dump_asm_bytes(raw,f)
+
