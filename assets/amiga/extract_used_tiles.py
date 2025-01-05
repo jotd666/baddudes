644c4ed03124d6e1988b2015d2040ec5756e695d
Jean-François Fabre <jotd666@gmail.com>
2025-01-05 16:39:23 +0100

    reorganized some data

--- assets/amiga/extract_used_tiles.py
+++ assets/amiga/extract_used_tiles.py
@@ -5,10 +5,11 @@ used_dict = {}
 dropped_tile_index = set()
 
 used_cluts_file = this_dir / "used_tile_cluts.json"
+used_graphics_dir = this_dir / "used_graphics"
 
 # get dude tiles (big dude pics in intro) and don't consider their tiles
 for i in range(2):
-    with open(this_dir / f"dudes_{i}_tiles","rb") as f:
+    with open(used_graphics_dir / f"dudes_{i}_tiles","rb") as f:
         f.seek(0x380)       # consider first tiles as used
         contents = f.read()
         for j in range(0,len(contents),2):
@@ -21,7 +22,7 @@ for i in range(2):
 ##    with open(used_cluts_file) as f:
 ##        used_dict = json.load(f)
 
-tiles_dir = this_dir / "tiles"
+tiles_dir = used_graphics_dir / "tiles"
 for n in os.listdir(tiles_dir):
     tiles_file = tiles_dir / n
     with open(tiles_file,"rb") as f:
