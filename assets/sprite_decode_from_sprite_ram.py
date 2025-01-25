40db1b968a442b65fcc61717aa1120537488d1b1
Jean-François Fabre <jotd666@gmail.com>
2025-01-25 18:49:57 +0100

    support for y flip

--- assets/sprite_decode_from_sprite_ram.py
+++ assets/sprite_decode_from_sprite_ram.py
@@ -2,18 +2,25 @@ import os,json,pathlib,struct
 
 this_dir = pathlib.Path(__file__).absolute().parent
 
-with open(r"karnov","rb") as f:
-    sprite_ram = f.read()
+def doit(filename):
+    print(">>>>>>>> ",filename)
+    with open(filename,"rb") as f:
+        sprite_ram = f.read()
 
-with open(os.path.join(this_dir,"sprite_code_names.json"),"r") as f:
-    sprite_name_code = {int(k,16):v for k,v in json.load(f)["codes_names"].items()}
+    with open(os.path.join(this_dir,"sprite_code_names.json"),"r") as f:
+        sprite_name_code = {int(k,16):v for k,v in json.load(f)["codes_names"].items()}
 
-for i in range(0,len(sprite_ram),8):
-    block = sprite_ram[i:i+8]
-    y,code,x,_ = struct.unpack_from(">HHHH",block)
-    if y & 0x8000 != 0:
-        attrs = y & 0xFE00
-        x &= 0x1FF
-        y &= 0x1FF
-        name = sprite_name_code.get(code,"unknown")
-        print(f"address={i+0xFFC000:06x}, x={x}, y={y}, code={code:x}, attrs={attrs:x}, name={name}")
+    for i in range(0,len(sprite_ram),8):
+        block = sprite_ram[i:i+8]
+        y,code,x,_ = struct.unpack_from(">HHHH",block)
+        if y & 0x8000 != 0:
+            attrs = y & 0xFE00
+            x &= 0x1FF
+            y &= 0x1FF
+            x = 240 - x
+            y = 240 - y
+            name = sprite_name_code.get(code,"unknown")
+            print(f"address={i+0xFFC000:06x}, x={x}, y={y}, code={code:x}, attrs={attrs:x}, name={name}")
+
+doit("dudejump")
+doit("dudejump_mame")
