199144153107cb48a9290b40a8d7c0435f3aa4f3
Jean-François Fabre <jotd666@gmail.com>
2025-02-19 23:04:35 +0100

    working on level 7

--- assets/amiga/convert_graphics.py
+++ assets/amiga/convert_graphics.py
@@ -768,7 +768,7 @@ def postprocess_game_osd_tiles(tileset,palette_index):
 generate_for_levels = [False]*8
 
 #generate_for_levels[1] = True
-generate_for_levels[2] = True
+#generate_for_levels[2] = True
 #generate_for_levels[3] = True
 #generate_for_levels[4] = True
 #generate_for_levels[6] = True
