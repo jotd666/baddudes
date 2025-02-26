237702e9544d6da58cc0d3a63054bef0c2a7b552
Jean-François Fabre <jotd666@gmail.com>
2025-02-26 10:56:20 +0100

    small gfx checks

--- assets/amiga/shared.py
+++ assets/amiga/shared.py
@@ -21,13 +21,29 @@ def load_grouped_dicts():
     # ATM manual lateral grouping of sprites (possibly already vertically grouped)
     # those are manual, but with the help of frame decode (sprite_decode.py) which analyzes
     # the whole set of game macro-objects
-    with open(this_dir / "macro_grouped_sprites.json") as f:
+    mgsf = this_dir / "macro_grouped_sprites.json"
+    with open(mgsf) as f:
         # load and convert to easily useable
         gd = json.load(f)
-        side_grouped_dict,vert_grouped_dict = [{int(k,16):[int(c,16) for c in v]
+        dicts = [{int(k,16):[int(c,16) for c in v]
                         if isinstance(v,list) else [int(v,16)] for k,v in gd[x].items()}
                         for x in ["horizontal","vertical"]]
-    return side_grouped_dict,vert_grouped_dict
+
+    # we need to check that values aren't found in keys as it would "loop" and hide sprite tiles
+    for x in dicts:
+        # collect keys
+        keys = set(x)
+        values = {e for v in x.values() for e in v}
+        shared_values = values.intersection(keys)
+        if shared_values:
+            sh = [hex(x) for x in shared_values]
+            raise Exception(f"in {mgsf}, values & key cross refs: {sh}")
+    # also check that there aren't any shared keys between dicts
+    shared_keys = set(dicts[0]) & set(dicts[1])
+    if shared_keys:
+        sh = [hex(x) for x in shared_keys]
+        raise Exception(f"Shared keys between horiz/vert grouping dicts: {sh}")
+    return dicts
 
 def asm2bin(source,dest):
     subprocess.run(["vasmm68k_mot","-nosym","-pic","-Fbin",source,"-o",dest],check=True,stdout=subprocess.DEVNULL)
