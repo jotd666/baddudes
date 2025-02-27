bc7461008be4b045a627b2f6d0aae1dfd2ff7a7f
Jean-François Fabre <jotd666@gmail.com>
2025-02-27 21:15:59 +0100

    more fixed colors

--- assets/amiga/convert_graphics.py
+++ assets/amiga/convert_graphics.py
@@ -847,8 +847,8 @@ generate_for_levels = [False]*9
 #generate_for_levels[3] = True
 #generate_for_levels[4] = True
 #generate_for_levels[5] = True
-generate_for_levels[6] = True
-#generate_for_levels[7] = True
+#generate_for_levels[6] = True
+generate_for_levels[7] = True
 #generate_for_levels[8] = True
 
 
