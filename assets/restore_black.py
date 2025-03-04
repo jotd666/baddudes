2fa4fa36ac4c6056ad0d13e6eeeee2479dede456
Jean-François Fabre <jotd666@gmail.com>
2025-03-04 21:55:40 +0100

    restored some black colors

--- assets/restore_black.py
+++ assets/restore_black.py
@@ -10,6 +10,7 @@ import PIL.Image,pathlib
 # (and called "fake black" here
 # 2) for other tile sheets, convert black to magenta (tranparent)
 # 3) apply black on stored coords
+# 4) keep track of the processed pixel to be able to perform a second pass with a possible different fake black
 
 this_dir = pathlib.Path(__file__).absolute().parent
 
@@ -19,25 +20,28 @@ black_color = (0,0,0)
 def gray(c):
     return (c,)*3
 
-def fix_black(pic_name,shadow_pic_name,fake_black):
+def fix_black(pic_name,shadow_pic_name,fake_black,processed_pixels):
     pic = PIL.Image.open(shadow_pic_name)
     # extract X,Y where pixels are supposed to be black
     really_black_coords = {(x,y) for x in range(pic.size[0]) for y in range(pic.size[1]) if pic.getpixel((x,y)) == fake_black}
 
+    processed_pixels = processed_pixels.copy()
 
     pic = PIL.Image.open(pic_name)
     for x in range(pic.size[0]):
         for y in range(pic.size[1]):
             # convert all black to transparent
-            if pic.getpixel((x,y)) == black_color:
+            if (x,y) not in processed_pixels and pic.getpixel((x,y)) == black_color:
                 pic.putpixel((x,y),transparent_color)
             # if it's an actual black color (revealed by another pic of the set
             # with a possible bogus palette), restore it
             if (x,y) in really_black_coords:
                 pic.putpixel((x,y),black_color)
-    return pic
+                processed_pixels.add((x,y))
 
-def fix_tileset(name,target_palette,black_reveal_palette_index,fake_black):
+    return pic,processed_pixels
+
+def fix_tileset(name,target_palette,black_reveal_palette_index,fake_black,processed_pixels=set()):
 
     tiles_dir = this_dir /"sheets"/f"{name}_black"
     tiles_out_dir = this_dir /"sheets"/name
@@ -49,12 +53,13 @@ def fix_tileset(name,target_palette,black_reveal_palette_index,fake_black):
     shadow_pic_name = f"pal_{black_reveal_palette_index:02x}.png"
     pic_name = f"pal_{target_palette:02x}.png"
 
-    pic = fix_black(tiles_dir / pic_name,tiles_dir / shadow_pic_name,fake_black)
+    pic,processed_pixels = fix_black(tiles_dir / pic_name,tiles_dir / shadow_pic_name,fake_black,processed_pixels)
 
     out_pic_name = tiles_out_dir / pic_name
     print(f"Saving {out_pic_name}")
     pic.save(out_pic_name)
 
+    return processed_pixels
 
 
 # it's too hard to dynamically change boss 4 palette with quantization
@@ -97,11 +102,18 @@ fix_tileset("sprites",0x3,0x9,gray(71))   # gray ninja
 fix_tileset("tiles_244000",0,4,(159,90,56))
 fix_tileset("sprites",0,0xA,gray(195))   # player tiles
 fix_tileset("sprites",2,0x9,gray(71))   # ninja tiles
-fix_tileset("sprites",0xF,0xD,(56,34,1))   # karnov tiles
-fix_tileset("sprites",0xF,0x9,gray(104))   # animal tiles
-fix_tileset("sprites",0xB,0x7,(230,230,0))   # tires
-fix_tileset("sprites",0x9,0x7,(230,230,0))   # tires
-fix_tileset("sprites",0x9,0xD,(56,34,1))   # karnov 2
+
+# chain 2 fixes from 2 different fake black sources, we have to remember which pixels
+# have been processed, else the second processing destroys the black pixels of the first one!
+processed_pixels = fix_tileset("sprites",0xF,0xD,(56,34,1))   # karnov tiles
+fix_tileset("sprites",0xF,0x9,gray(104),processed_pixels)   # animal tiles
+
+fix_tileset("sprites",0xB,0x7,(230,230,0))   # car tires
+
+# chain 2 fixes from 2 different fake black sources, we have to remember which pixels
+# have been processed, else the second processing destroys the black pixels of the first one!
+processed_pixels = fix_tileset("sprites",0x9,0xD,(56,34,1))   # karnov 2
+fix_tileset("sprites",0x9,0x7,(230,230,0),processed_pixels)   # truck tires
 fix_tileset("sprites",0xA,0x7,(230,230,0))   # cars
 fix_tileset("sprites",0xC,0x7,(230,230,0))   # cars
 fix_tileset("sprites",0x5,0xD,(56,34,1))   # ninja nails
