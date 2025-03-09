8ec7afca23b0f12f2724d872994201be4c715e35
Jean-François Fabre <jotd666@gmail.com>
2025-03-09 11:32:50 +0100

    fixed a few things, hoping they stay fixed!

--- assets/amiga/convert_graphics.py
+++ assets/amiga/convert_graphics.py
@@ -869,13 +869,13 @@ generate_for_levels = [False]*9
 
 ##generate_for_levels[0] = True
 ##generate_for_levels[1] = True
-##generate_for_levels[2] = True
+generate_for_levels[2] = True
 ##generate_for_levels[3] = True
 ##generate_for_levels[4] = True
-generate_for_levels[5] = True
-##generate_for_levels[6] = True
+#generate_for_levels[5] = True
+#generate_for_levels[6] = True
 ##generate_for_levels[7] = True
-generate_for_levels[8] = True
+#generate_for_levels[8] = True
 
 
 # set to "False" for faster operation when working on game sprite/tiles
